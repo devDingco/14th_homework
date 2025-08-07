b6bf69bf9ea31c7f648be84d38e9ddcb1827322a
gunmohkoo1 <sei2kduv@gmail.com>
2025-08-07 17:51:03 +0900

    Add files via upload
    골격

---
+++ hw/Corp_name.js
@@ -0,0 +1,4 @@
+window.onload = () => {
+            const 회사명 = prompt("회사명을 입력하세요")
+            document.getElementById("회사명입력하는곳").innerText = 회사명
+        }
---
+++ hw/input.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+            const 나의이름 = prompt("이름을 입력하세요")
+            document.getElementById("이름입력하는곳").innerText = 나의이름
+        }
+
+window.onload = () => {
+           const 나의이름 = prompt("이름을 입력하세요")
+           document.getElementById("이름입력하는곳").innerText = 나의이름
+}
+window.onload = () => {
+           const 나의이름 = prompt("이름을 입력하세요")
+           document.getElementById("이름입력하는곳").innerText = 나의이름
+}
+window.onload = () => {
+           const 나의이름 = prompt("이름을 입력하세요")
+           document.getElementById("이름입력하는곳").innerText = 나의이름
+}
