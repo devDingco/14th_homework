3d2bfeaa1e3adf9cdc899b5d9114a4f5f916c039
yunjae Kim <yoonk1228@gmail.com>
2025-08-11 19:01:27 +0900

    feat: 상세페이지구현

--- css-js-master/function/toLocalStorage.js
+++ css-js-master/function/toLocalStorage.js
@@ -1,18 +1,10 @@
 // 로컬스토리지에 일기 저장
 const toLocalStorage = (arr) => {
-    const contentArr = arr // [{number, feel, title, detail}... ]
-    
-    // todo - innerHtml 함수로 이동
-    const currentDate = 
-        `
-        ${new Date().getFullYear()}. ${new Date().getMonth() + 1}. ${new Date().getDate()}
-        `
+    const contentArr = arr // [{number, date, feel, title, detail}... ]
     
     localStorage.setItem("일기콘텐츠", JSON.stringify(contentArr))
 
     console.log(contentArr)
-    console.log(currentDate)
-    
     // innerHTML 수정 함수 실행 (일기쓰기 초기화, 메인컨텐ㅊ 수정)
     
     alert('성공적으로 등록 되었습니다!')
