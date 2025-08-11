94dd14cba089d6c2aee1c2a413c531fc8de11b01
Daram0o0 <c30728535@gmail.com>
2025-08-12 02:25:40 +0900

    chore: 댓글 기능 추가에 따른 구조 일부 수정

--- mood-diary/js/userDiary.js
+++ mood-diary/js/userDiary.js
@@ -31,14 +31,16 @@ const getDiaryById = (id) => {
 //ERROR: 현재, index.html과 detailPage.html에서 userDiary.js 파일을 동시 공유하고 있기 때문에 콘솔에 에러메시지기 찍힘
 // -> html별로 js 파일 나누기 고려 중
 //diaryId 기반 해당하는 값 html에 뿌리기
-const { mood, title, contents, icon, color, date } = formattedDiary(getDiaryById(diaryId)[0])
+const { mood, title, contents, icon, color, date,comments } = formattedDiary(getDiaryById(diaryId)[0])
 document.getElementById("detailTitle").innerText = title
 document.getElementById("detailIcon").src = icon
 document.getElementById("detailMood").innerText = mood
 document.getElementById("detailContents").innerText = contents
 document.getElementById("detailDate").innerText = `${date} 작성`
 document.getElementById("detailMood").style.color = color
-
+addCommentsOnArea(comments)
+scrollToComments()
+// location.reload()
 //수정 클릭시 "detail-main"화면 숨기기 + "main__diary-form"화면 보이기 + 내부내용 넣어두기
 
 const viewModifyContent = () => {
@@ -56,6 +58,9 @@ const viewModifyContent = () => {
   }
   document.querySelector("#title").value = title
   document.querySelector("#contents").value = contents
+
+  document.getElementById("detail-comments-input-input").disabled = true
+  document.getElementById("detail-comments-input-button").disabled = true
 }
 
 //수정페이지 -> 상세페이지로 이동하는 함수
@@ -64,7 +69,23 @@ const moveBack = () => {
 }
 
 //INFO: diaryList "[UPDATE]" 함수
-//form.js 재활용
+//form.js 재사용
 
 // mood가 전체가 아니게 선택된 경우,
 // 있던 틀 버리기 + 새 틀 추가 + 필터된 다이어리들 추가
+
+//INFO: diaryList "DELETE" 함수
+
+// const deleteDiary = () => {
+//   alert("클릭됨")
+//   // event.stopPropagation()
+//   // console.log(id)
+//   // alert(id)
+//   // const ok = confirm(`${id}번째 일기를 삭제하시겠습니까?`)
+//   // if (ok) {
+//   //   diaryList.filter((el)=>el.id !== +id)
+//   // }
+// }
+
+//INFO: diaryList "FILTER" 함수
+// test function
