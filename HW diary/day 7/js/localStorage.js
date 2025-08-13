ab338fe18915edec6bf1b20f1df7f291e77cf2a2
gunmohkoo1 <sei2kduv@gmail.com>
2025-08-13 11:23:48 +0900

    수치단위 변경
    px 만 rem으로 교체. 
    교체후 배치 다깨졌네요.

---
+++ HW diary/day 7/js/localStorage.js
@@ -0,0 +1,80 @@
+window.onload = () => {
+ console.log("1")
+};
+
+const selectElement = document.getElementById('필터색변경');
+selectElement.classList.add('body__search__selection_text')
+window.addEventListener("scroll", () => {
+    const scrollDownLength = window.scrollY;
+        if(scrollDownLength > 0) {
+            selectElement.classList.remove('body__search__selection_text');
+            selectElement.classList.add('selection_text-reverse');
+        } else {
+            selectElement.classList.remove('selection_text-reverse');
+            selectElement.classList.add('body__search__selection_text');
+        }
+    }
+);
+
+const 삭제기능 = (event) => {
+    
+    event.stopPropagation()
+
+}
+
+
+
+
+
+
+
+// const diaryList = [];
+
+// const registerDiary = () => {
+//     const today_date = new Date()
+
+//     const dateDetail = {
+//         year: today_date.getFullYear(),
+//         month: (today_date.getMonth()+1).toString().padStart(2,"0"),
+//         date: (today_date.getDate()).toString().padStart(2, "0"),
+//     };
+
+//     const dateStorage = dateDetail.map((el, index) => ` ${el, year}.${el, month}.${el, date} ` ).join("");
+//     const titleStorage = window.document.getElementById("id_diary__title").value;
+//     const contextStorage = window.document.getElementById("id_diary__contextarea").value;
+
+//     let 행복변수 = window.document.getElementById("happy");
+//     let 슬픔변수 = window.document.getElementById("sad");
+//     let 놀람변수 = window.document.getElementById("shocked");
+//     let 화남변수 = window.document.getElementById("angry");
+//     let 기타변수 = window.document.getElementById("etccc");
+
+// };
+// //일기 목록화
+// const diaryStorage = {
+//     title: titleStorage,
+//     text: contextStorage,
+//     wrdate: dateStorage,
+// };
+// // 일기 배열에 넣기
+// diaryList.push(diaryStorage);
+// // 일기번호 따기
+// const diaryNumbering = diaryList.length-1;
+
+// const 기존그림이랑본문 = window.document.getElementById(picbox).innerHTML;
+
+// const 행복 = <img class="emoticon" src="./assets/icons/happy.png" />
+// const 슬픔 = <img class="emoticon" src="./assets/icons/sad.png" />
+// const 놀람 = <img class="emoticon" src="./assets/icons/suprise.png" />
+// const 화남 = <img class="emoticon" src="./assets/icons/angry.png" />
+// const 기타 = <img class="emoticon" src="./assets/icons/etc.png" />
+
+
+// const 새로등록된그림이랑본문 = ` 
+//     ${행복변수 === true? < 행복 : ""}
+//     ${슬픔변수 === true? < 슬픔 : ""}
+//     ${놀람변수 === true? < 놀람 : ""}
+//     ${화남변수 === true? < 화남 : ""}
+//     ${기타변수 === true? < 기타 : ""}
+
+// `;
---
+++ HW diary/day 7/nodata/filter.js
@@ -0,0 +1,3 @@
+const filteringFn = (event) => {
+    
+}
