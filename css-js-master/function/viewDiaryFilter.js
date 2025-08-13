a9e7640492c7e1d7240a326c3f161e2397dcf702
yunjae Kim <yoonk1228@gmail.com>
2025-08-13 11:19:30 +0900

    feat: 필터 전체 기능 추가

--- css-js-master/function/viewDiaryFilter.js
+++ css-js-master/function/viewDiaryFilter.js
@@ -1,10 +1,11 @@
 const viewDiaryFilter = () => {
     const getFilterFeel = document.getElementById("diary_filter").value
-
     const localStorageArr = JSON.parse(localStorage.getItem("일기콘텐츠"))
-    // console.log(localStorageArr)
-    console.log(getFilterFeel)
-    const filterContentArr = localStorageArr.filter(v => v.feel === getFilterFeel).map(v => v)
-    
-    document.getElementById("diary_box_container").innerHTML = makeMainHtml(filterContentArr)
+
+    if (getFilterFeel === "전체") {
+        document.getElementById("diary_box_container").innerHTML = makeMainHtml(localStorageArr)
+    } else {
+        const filterContentArr = localStorageArr.filter(v => v.feel === getFilterFeel).map(v => v)
+        document.getElementById("diary_box_container").innerHTML = makeMainHtml(filterContentArr)
+    }
 }
