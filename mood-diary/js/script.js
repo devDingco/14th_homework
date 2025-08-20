ad0aeea885a3f031be1fe3369bd91b4ea6379055
Daram0o0 <c30728535@gmail.com>
2025-08-20 09:46:58 +0900

    feat: 페이지네이션 독립 기능 완성

--- mood-diary/js/script.js
+++ mood-diary/js/script.js
@@ -1,25 +1,26 @@
 //window가 로드시, 감정 일기장 리스트 출력
 window.onload = function () {
-  const diaryList = getDiaryList(); 
+  const diaryList = getDiaryList()
   addCardsOnGallery(diaryList)
-
-
-  const message = sessionStorage.getItem(TOAST_KEY);
+  showPageBtn(startPage)
+  displayPage(startPage)
+  // pagingDiaryList(diaryList, startPage)
+  const message = sessionStorage.getItem(TOAST_KEY)
   if (message) {
     alert(message)
     // ERROR: 토스트메시지 활용 시, submit 제출 후 제출 or 삭제 후 제출 한 번 씹힘
     // toastMessage(message);
-    sessionStorage.removeItem(TOAST_KEY);
+    sessionStorage.removeItem(TOAST_KEY)
   }
 }
 
 // test function
 const test = () => {
-  alert("클릭되었습니다.")
+  alert('클릭되었습니다.')
 }
 
 const scrollToTop = () => {
-	window.scrollTo({ top: 0, behavior: "smooth" })
+  window.scrollTo({ top: 0, behavior: 'smooth' })
 }
 
 // let scrolling
@@ -34,10 +35,9 @@ const scrollToTop = () => {
 // }
 // window.addEventListener('scroll', changeFilterColor)
 
-
 const scrollToComments = () => {
-  const scrollArea = document.getElementById("comments")
-  scrollArea.scrollIntoView({behavior:"smooth",block:"start"})
+  const scrollArea = document.getElementById('comments')
+  scrollArea.scrollIntoView({ behavior: 'smooth', block: 'start' })
 }
 // window.addEventListener("scroll",scrollToComments)
 // 클립보드 저장함수
@@ -50,9 +50,9 @@ const toastMessage = (contents) => {
   const body = document.body
   body.innerHTML += `<p class="toast-bottom">${contents}</p>`
 
-  document.querySelector(".toast-bottom").style = "display:block"
+  document.querySelector('.toast-bottom').style = 'display:block'
   setTimeout(() => {
-    document.querySelector(".toast-bottom").style = "display:none"
+    document.querySelector('.toast-bottom').style = 'display:none'
   }, 2000)
 }
 
