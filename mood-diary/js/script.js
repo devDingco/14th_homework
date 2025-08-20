5137035f943c648297ee381808d6479d3b81cec2
Daram0o0 <c30728535@gmail.com>
2025-08-20 15:22:19 +0900

    refactor: 함수 일부 재배치, 코드 구조 정리

--- mood-diary/js/script.js
+++ mood-diary/js/script.js
@@ -1,10 +1,9 @@
 //window가 로드시, 감정 일기장 리스트 출력
-window.onload = function () {
+window.onload = () => {
   const diaryList = getDiaryList()
   addCardsOnGallery(diaryList)
-  showPageBtn(startPage)
-  displayPage(startPage)
-  // pagingDiaryList(diaryList, startPage)
+  render()
+
   const message = sessionStorage.getItem(TOAST_KEY)
   if (message) {
     alert(message)
@@ -12,50 +11,4 @@ window.onload = function () {
     // toastMessage(message);
     sessionStorage.removeItem(TOAST_KEY)
   }
-}
-
-// test function
-const test = () => {
-  alert('클릭되었습니다.')
-}
-
-const scrollToTop = () => {
-  window.scrollTo({ top: 0, behavior: 'smooth' })
-}
-
-// let scrolling
-// const changeFilterColor = () => {
-//   const dropdown = document.getElementById("dropdown")
-//   dropdown.style = "background-color: var(--color-black); color:var(--color-white);"
-
-//   clearTimeout(scrolling);
-//   scrolling = setTimeout(() => {
-//     dropdown.style = "background-color: var(--color-white);color: var(--color-black);"
-//   },100)
-// }
-// window.addEventListener('scroll', changeFilterColor)
-
-const scrollToComments = () => {
-  const scrollArea = document.getElementById('comments')
-  scrollArea.scrollIntoView({ behavior: 'smooth', block: 'start' })
-}
-// window.addEventListener("scroll",scrollToComments)
-// 클립보드 저장함수
-const copyContents = () => {
-  navigator.clipboard.writeText(contents)
-  toastMessage('내용이 복사되었습니다.')
-}
-
-const toastMessage = (contents) => {
-  const body = document.body
-  body.innerHTML += `<p class="toast-bottom">${contents}</p>`
-
-  document.querySelector('.toast-bottom').style = 'display:block'
-  setTimeout(() => {
-    document.querySelector('.toast-bottom').style = 'display:none'
-  }, 2000)
-}
-
-const toggleDarkMode = () => {
-  document.body.classList.toggle('darkmode')
 }
--- mood-diary/js/userDiary.js
+++ mood-diary/js/userDiary.js
@@ -1,81 +1,33 @@
-//INFO: diaryList CRUD
-
-//로컬스토리지 저장하는 함수
-const storeDiaryList = (array) => {
-  // 타입체크후?????, 
-  array = array?.length > 0 ? array : []
-  localStorage.setItem(DIARY_KEY, JSON.stringify(array))
-}
-
-//로컬스토리지 가져오는 함수
-const getDiaryList = () => {
-  return JSON.parse(localStorage.getItem(DIARY_KEY) ?? '[]');
-}
-
 let diaryList = getDiaryList() ?? []
 
 //queryString에서 id값 가져오기
 const queryString = location.search
 const findQueryString = new URLSearchParams(queryString)
-let diaryId = findQueryString.get("diaryId")
-
+let diaryId = findQueryString.get('diaryId')
 
 // diaryId 기반 해당하는 다이어리 정보 가져오기
 const getDiaryById = (id) => {
-  return diaryList.filter(diary => diary.id === +id)
+  return diaryList.filter((diary) => diary.id === +id)
 }
-
 
 /** DetailPage(Modify function) **/
 //ERROR: 현재, index.html과 detailPage.html에서 userDiary.js 파일을 동시 공유하고 있기 때문에 콘솔에 에러메시지기 찍힘
 // -> html별로 js 파일 나누기 고려 중
 //diaryId 기반 해당하는 값 html에 뿌리기
-const { mood, title, contents, icon, color, date, comments } = formattedDiary(getDiaryById(diaryId)[0])
-const header = document.getElementById("detailHeader")
-header.innerText = "< 일기 상세" //TODO: 반응형에서만 되어야 함?
-header.addEventListener("click", () => {
+const { mood, title, contents, icon, color, date, comments } = formattedDiary(
+  getDiaryById(diaryId)[0]
+)
+const header = document.getElementById('detailHeader')
+header.innerText = '< 일기 상세' //TODO: 반응형에서만 되어야 함?
+header.addEventListener('click', () => {
   location.replace('./index.html')
 })
-document.getElementById("detailTitle").innerText = title
-document.getElementById("detailIcon").src = icon
-document.getElementById("detailMood").innerText = mood
-document.getElementById("detailContents").innerText = contents
-document.getElementById("detailDate").innerText = `${date} 작성`
-document.getElementById("detailMood").style.color = color
+document.getElementById('detailTitle').innerText = title
+document.getElementById('detailIcon').src = icon
+document.getElementById('detailMood').innerText = mood
+document.getElementById('detailContents').innerText = contents
+document.getElementById('detailDate').innerText = `${date} 작성`
+document.getElementById('detailMood').style.color = color
 
 addCommentsOnArea(comments)
 scrollToComments()
-
-
-//수정 클릭시 "detail-main"화면 숨기기 + "main__diary-form"화면 보이기 + 내부내용 넣어두기
-
-const viewModifyContent = () => {
-  const header = document.getElementById("detailHeader")
-  header.innerText = "〈 일기 수정" //TODO: 반응형에서만 되어야 함?
-  header.addEventListener("click", () => {
-    location.reload()
-  }) //반응형에서만..?
-
-  document.querySelector(".detail-main").style.display = "none"
-  document.querySelector("#detail-modify-form").style.display = "flex"
-
-  //TODO: 변수네이밍 변경 및 forEach, Arry.from 기능 정리
-  const temp = Array.from(document.querySelectorAll('input[name="mood"]'))
-  // forEach 동작하지 않는 이유?
-  for (let i = 0; i < temp.length; i++){
-    if (getMoodLabel(temp[i].id) === mood) {
-      temp[i].checked = true
-    }
-  }
-  document.querySelector("#title").value = title
-  document.querySelector("#contents").value = contents
-  document.getElementById("detail-comments-input-input").placeholder = "수정중일땐 회고를 작성할 수 없어요."
-  document.getElementById("detail-comments-input-input").disabled = true
-  document.getElementById("detail-comments-input-button").disabled = true
-  
-}
-
-//수정페이지 -> 상세페이지로 이동하는 함수
-const moveBack = () => {
-  location.reload()
-}
