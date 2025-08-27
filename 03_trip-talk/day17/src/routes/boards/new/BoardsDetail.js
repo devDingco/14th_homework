59e697bf0a639e35f275feb51be7ce3e2b1ab7ab
Daram0o0 <c30728535@gmail.com>
2025-08-27 22:15:09 +0900

    refactor: BoardsDetail 상수를 mock 데이터로 추출

--- 03_trip-talk/day17/src/routes/boards/new/BoardsDetail.js
+++ 03_trip-talk/day17/src/routes/boards/new/BoardsDetail.js
@@ -9,55 +9,42 @@ import {
   ListIcon,
   EditIcon,
 } from '../../../assets/icons'
-import { postImg1 } from '../../../assets/images'
-
-const tempTitle = `살어리 살어리랏다 쳥산(靑山)애 살어리랏다멀위랑 ᄃᆞ래랑 먹고 쳥산(靑山)애 살어리랏다얄리얄리 얄랑셩 얄라리 얄라`
-const tempContents = `살겠노라 살겠노라. 청산에 살겠노라.
-머루랑 다래를 먹고 청산에 살겠노라.
-얄리얄리 얄랑셩 얄라리 얄라
-
-우는구나 우는구나 새야. 자고 일어나 우는구나 새야.
-너보다 시름 많은 나도 자고 일어나 우노라.
-얄리얄리 얄라셩 얄라리 얄라
-
-갈던 밭(사래) 갈던 밭 보았느냐. 물 아래(근처) 갈던 밭 보았느냐
-이끼 묻은 쟁기를 가지고 물 아래 갈던 밭 보았느냐.
-얄리얄리 얄라셩 얄라리 얄라
-
-이럭저럭 하여 낮일랑 지내 왔건만
-올 이도 갈 이도 없는 밤일랑 또 어찌 할 것인가.
-얄리얄리 얄라셩 얄라리 얄라
-
-어디다 던지는 돌인가 누구를 맞히려던 돌인가.
-미워할 이도 사랑할 이도 없이 맞아서 우노라.
-얄리얄리 얄라셩 얄라리 얄라
-
-살겠노라 살겠노라. 바다에 살겠노라.
-나문재, 굴, 조개를 먹고 바다에 살겠노라.
-얄리얄리 얄라셩 얄라리 얄라
-
-가다가 가다가 듣노라. 에정지(미상) 가다가 듣노라.
-사슴(탈 쓴 광대)이 솟대에 올라서 해금을 켜는 것을 듣노라.
-얄리얄리 얄라셩 얄라리 얄라
-
-가다 보니 배불룩한 술독에 독한 술을 빚는구나.
-조롱박꽃 모양 누룩이 매워 (나를) 붙잡으니 내 어찌 하리이까.[1]
-얄리얄리 얄라셩 얄라리 얄라`
+import { posts } from '../../../mocks/posts.mock'
+import { Link, useParams } from 'react-router'
 
 const BoardsDetail = () => {
+  const { id = '1' } = useParams()
+  const post = posts.find((p) => +p.id === +id)
+
+  if (!id) {
+    return (
+      <div>
+        잘못된 접근입니다. <Link to="/">홈으로</Link>
+      </div>
+    )
+  }
+
+  if (!post) {
+    return (
+      <div>
+        게시글을 찾을 수 없어요. <Link to="/">홈으로</Link>
+      </div>
+    )
+  }
+
   return (
     <div className="container">
       <div className="detail-post">
-        <Header title={tempTitle} />
+        <Header title={post.title} />
 
         {/* details option */}
         <div className="detail-post-details">
           <div className="detail-post-info">
             <div className="detail-post-profile">
               <ProfileIcon />
-              <p>{'홍길동'}</p>
+              <p>{post.writer}</p>
             </div>
-            <p>2024.11.11</p>
+            <p>{post.date}</p>
           </div>
           <hr />
           <div className="detail-post-func">
@@ -67,25 +54,25 @@ const BoardsDetail = () => {
         </div>
 
         {/* images */}
-        <img src={postImg1} className="detail-post-image" />
+        <img src={post.images[0]} className="detail-post-image" />
 
         {/* contents */}
-        <div className="detail-post-contents">{tempContents}</div>
+        <div className="detail-post-contents">{post.contents}</div>
 
         {/* youtube link */}
         <div className="detail-post-youtube">
-          <iframe src="https://www.youtube.com/embed/sQNvtxKMX3w"></iframe>
+          <iframe src={post.youtubeLink}></iframe>
         </div>
 
         {/* dis-like button */}
         <div className="detail-post-likes">
           <div className="detail-post-like">
             <BadIcon />
-            <p>24</p>
+            <p>{post.dislikes}</p>
           </div>
           <div className="detail-post-like">
             <GoodIcon />
-            <p>12</p>
+            <p>{post.likes}</p>
           </div>
         </div>
 
