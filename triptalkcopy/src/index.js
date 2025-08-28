86c08740f755f67767a614b4d1543e11cbd1e832
Jiyoon-Yang <jiyoon3522@naver.com>
2025-08-28 09:31:41 +0900

    [250828]트립토크

---
+++ triptalkcopy/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+import { createBrowserRouter, RouterProvider } from "react-router";
+import PostForm from "./App";
+import BoardsNew from "./routes/boards/new/BoardsNew";
+import BoardsDetail from "./routes/boards/new/BoardsDetail";
+
+const pageList = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/boards/new", element: <BoardsNew /> },
+  { path: "/boards/detail", element: <BoardsDetail /> },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={pageList} />);
