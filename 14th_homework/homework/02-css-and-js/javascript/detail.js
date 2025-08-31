1f8c813993a482f457bfedbbf40178959ef89f3c
JY_Han <jy@JY-Hanui-MacBookAir.local>
2025-09-01 02:00:33 +0900

    Add 14th_homework folder as normal folder

---
+++ 14th_homework/homework/02-css-and-js/javascript/detail.js
@@ -0,0 +1,244 @@
+window.onload = () => {
+    // 1. 주소에서 일기번호 가져오기
+    const 쿼리스트링 = window.location.search;
+    const 잘게나누어담은통 = new URLSearchParams(쿼리스트링);
+    const 일기번호 = 잘게나누어담은통.get("number");
+  
+    // 2. 스토리지에 저장된 일기목록 가져오기
+    const 스토리지에저장된일기목록 =
+      window.localStorage.getItem("민지의일기목록") ?? "[]";
+    const 일기목록 = JSON.parse(스토리지에저장된일기목록);
+  
+    // 3. 일기목록에서 현재일기번호 가져오기
+    const 일기담는통 = 일기목록[일기번호];
+  
+    let 기분 = 일기담는통.기분;
+    let 기분메시지;
+    switch (기분) {
+      case "행복":
+        기분메시지 = "행복해요";
+        이미지경로 = "./assets/images/happy_imoji.png";
+        글자색 = "#EA5757";
+        break;
+      case "슬픔":
+        기분메시지 = "슬퍼요";
+        이미지경로 = "./assets/images/sad_imoji.png";
+        글자색 = "#28B4E1";
+        break;
+      case "놀람":
+        기분메시지 = "놀랐어요";
+        이미지경로 = "./assets/images/surprise_imoji.png";
+        글자색 = "#D59029";
+        break;
+      case "화남":
+        기분메시지 = "화나요";
+        이미지경로 = "./assets/images/mad_imoji.png";
+        글자색 = "#777";
+        break;
+      default:
+        기분메시지 = "기타";
+        이미지경로 = "./assets/images/think_imoji.png";
+        글자색 = "#A229ED";
+        break;
+    }
+  
+    // 4. 일기상세내용 화면에 그리기
+    window.document.getElementById("HTML_일기상세제목보여주는곳").innerHTML =
+      일기담는통.제목;
+    window.document.getElementById("HTML_일기기분보여주는곳").innerHTML =
+      기분메시지;
+    window.document.getElementById("HTML_일기기분보여주는곳").style.color =
+      글자색;
+    window.document.getElementById("HTML_일기상세내용보여주는곳").innerHTML =
+      일기담는통.내용;
+    window.document.getElementById("HTML_기분이미지보여주는곳").src = 이미지경로;
+    window.document.getElementById("HTML_기분이미지보여주는곳").alt = 기분메시지;
+    window.document.getElementById("HTML_날짜보여주는곳").innerHTML =
+      일기담는통.작성일;
+
+    JS_댓글그리기기능(); 
+
+    const 댓글목록영역 = document.getElementById("HTML_댓글목록영역");
+    if (댓글목록영역) {
+      댓글목록영역.scrollIntoView({behavior: "smooth"});
+    }
+    
+};
+  
+const JS_수정하기기능 = () => {
+  // 1. 주소에서 일기번호 가져오기
+  const 쿼리스트링 = window.location.search;
+  const 잘게나누어담은통 = new URLSearchParams(쿼리스트링);
+  const 일기번호 = 잘게나누어담은통.get("number");
+  
+  // 2. 수정페이지로 이동하기
+  window.location.href = `./edit.html?number=${일기번호}`;
+};
+  
+const JS_삭제하기기능 = () => {
+  모달열기("일기삭제모달그룹ID");
+};
+
+const JS_일기삭제확인기능 = () =>{
+  const 쿼리스트링 = window.location.search;
+  const 잘게나누어담은통 = new URLSearchParams(쿼리스트링);
+  const 일기번호 = 잘게나누어담은통.get("number");
+  현재삭제할일기번호 = 일기번호;
+
+  const 스토리지에저장된일기목록=
+    window.localStorage.getItem("민지의일기목록") ?? "[]"
+  const 일기목록 = JSON.parse(스토리지에저장된일기목록);
+  
+  if(현재삭제할일기번호 !== null) {
+    // 1. 클릭된 일기번호 삭제하기
+    const 삭제후일기목록 = 일기목록.filter(
+      (_, index) => index !== parseInt(일기번호, 10)
+    );
+    // 2. 삭제된 일기목록 다시 저장하기
+    window.localStorage.setItem(
+      "민지의일기목록",
+      JSON.stringify(삭제후일기목록)
+    );
+
+    // 3. 메인페이지로 이동
+    location.replace("./index.html");
+  }
+}
+
+const JS_댓글달기기능 = () => {
+  const 쿼리스트링 = window.location.search;
+  const 잘게나누어담은통 = new URLSearchParams(쿼리스트링);
+  const 일기번호 = 잘게나누어담은통.get("number"); 
+  
+  const 스토리지에저장된일기목록=
+    window.localStorage.getItem("민지의일기목록") ?? "[]"
+  const 일기목록 = JSON.parse(스토리지에저장된일기목록);
+
+  const 댓글내용담는통 =
+    window.document.getElementById("HTML_댓글입력창").value;
+
+  const 원래있던댓글목록 = 일기목록[일기번호].댓글목록;
+
+  const date = new Date();
+  // const options = {
+  //   year: "numeric",
+  //   month: "2-digit",
+  //   day: "2-digit",
+  //   timeZone: "Asia/Seoul",
+  // };
+
+  // const 날짜담는통 = date
+  //   .toLocaleDateString("ko-KR", options)
+  //   .slice(0, -1);
+
+  const options = {
+    year: date.getFullYear(),
+    month: (date.getMonth() + 1).toString().padStart(2, "0"),
+    date: date.getDate().toString().padStart(2, "0"),
+  };
+
+  const 날짜담는통 = options.year + ". " + options.month + ". " + options.date;
+
+  if (원래있던댓글목록 === undefined) {
+    일기목록[일기번호].댓글목록 = [
+      {
+        댓글내용: 댓글내용담는통,
+        작성일: 날짜담는통,
+      },
+    ];
+  } else {
+    일기목록[일기번호].댓글목록.push({
+      댓글내용: 댓글내용담는통,
+      작성일: 날짜담는통,
+    });
+  }
+  window.localStorage.setItem("민지의일기목록",JSON.stringify(일기목록));
+
+  window.document.getElementById("HTML_댓글입력창").value = "";
+
+  JS_댓글그리기기능();
+};
+
+const JS_댓글그리기기능 = () => {
+  const 쿼리스트링 = window.location.search;
+  const 잘게나누어담은통 = new URLSearchParams(쿼리스트링);
+  const 일기번호 = 잘게나누어담은통.get("number");
+
+  const 스토리지에저장된일기목록=
+    window.localStorage.getItem("민지의일기목록") ?? "[]"
+  const 일기목록 = JSON.parse(스토리지에저장된일기목록);
+
+  const 일기담는통 = 일기목록[일기번호];
+
+  const 댓글목록 = 일기담는통.댓글목록 ?? [];
+
+  let HTML_새로운댓글목록 = "";
+  댓글목록.forEach((댓글, index) => {
+    const hasDivider = index < 댓글목록.length - 1;
+
+    HTML_새로운댓글목록 += `
+      <div class="comment_item">
+        <div class="comment_content">${댓글.댓글내용}</div>
+        <div class="comment_date">${댓글.작성일}</div>
+      </div>
+      ${hasDivider ? '<hr class="comment_divider" />' : ''}
+    `;
+  });
+  document.getElementById("HTML_댓글목록영역").innerHTML = HTML_새로운댓글목록;
+};
+
+const JS_뒤로가기기능 = () => {
+  window.history.back();
+};
+
+
+window.addEventListener("scroll", () => {
+  const 화면위에서푸터위까지길이 = document
+    .getElementById("HTML_푸터")
+    .getBoundingClientRect().top;
+  const 보이는화면길이 = window.innerHeight;
+
+  // 1. 푸터가 보일 때는, 화면과 상관없이 사진에 고정시키기
+  if (보이는화면길이 >= 화면위에서푸터위까지길이) {
+    document.getElementById("HTML_플로팅버튼").style = `
+      position: relative;
+      bottom: 0;
+      left: 97%;
+    `;
+
+    // 2. 푸터가 안보일 때는, 사진과 상관없이 화면에 고정시키기
+  } else {
+    document.getElementById("HTML_플로팅버튼").style = `
+      position: fixed;
+      bottom: 4rem;
+      right: 2rem;
+    `;
+  }
+});
+
+
+const JS_스크롤위로기능 = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+};
+
+const JS_복사하기기능 = () => {
+  // 1. 주소에서 일기번호 가져오기
+  const 쿼리스트링 = window.location.search;
+  const 잘게나누어담은통 = new URLSearchParams(쿼리스트링);
+  const 일기번호 = 잘게나누어담은통.get("number");
+
+  // 2. 스토리지에 저장된 일기목록 가져오기
+  const 스토리지에저장된일기목록 =
+    window.localStorage.getItem("민지의일기목록") ?? "[]";
+  const 일기목록 = JSON.parse(스토리지에저장된일기목록);
+
+  // 3. 일기목록에서 현재일기번호 가져오기
+  const 일기담는통 = 일기목록[일기번호];
+
+  // 4. 일기내용 클립보드에 복사하기
+  navigator.clipboard.writeText(일기담는통.내용);
+  JS_복사토스트메시지실행기능();
+};
---
+++ 14th_homework/homework/03-day19/src/app/boards/detail/page.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import React from 'react';
+import styles from "./styles.module.css";
+
+const BoardsDetail = () => { 
+    return(
+        <div className={styles.layout}>
+            <div className={styles.container}>
+                <div className={styles.detailSubjectText}>
+                    살어리 살어리랏다 쳥산(靑山)애 살어리랏다멀위랑 ᄃᆞ래랑 먹고
+                    쳥산(靑山)애 살어리랏다얄리얄리 얄랑셩 얄라리 얄라
+                </div>
+
+                <div className={styles.detailMetadataContainer}>
+                    <div className={styles.detailRowFlex}>
+                        <div className={styles.detailMetadataProfile}>
+                            <img src="/images/userprofile_default.png" alt="프로필"/>
+                            <div>홍길동</div>
+                        </div>
+                        <div>2024.11.11</div>
+                    </div>
+                    <hr className={styles.detailBorder}/>
+                    <div className={styles.detailMetadataIconContainer}>
+                        <img src="/images/link_icon.png" alt="링크아이콘" />
+                        <img src="/images/location_icon.png" alt="위치아이콘" />
+                    </div>
+                </div>
+
+                <img className={styles.detailContentContainer} src="/images/post_image.png" alt="청산사진"/>
+
+                <div className={styles.detailContentText}>
+                    살겠노라 살겠노라. 청산에 살겠노라. <br />
+                    머루랑 다래를 먹고 청산에 살겠노라. <br />
+                    얄리얄리 얄랑셩 얄라리 얄라 <br />
+                    <br />
+                    우는구나 우는구나 새야. 자고 일어나 우는구나 새야. <br />
+                    너보다 시름 많은 나도 자고 일어나 우노라. <br />
+                    얄리얄리 얄라셩 얄라리 얄라 <br />
+                    <br />
+                    갈던 밭(사래) 갈던 밭 보았느냐. 물 아래(근처) 갈던 밭 보았느냐 <br />
+                    이끼 묻은 쟁기를 가지고 물 아래 갈던 밭 보았느냐.<br />
+                    얄리얄리 얄라셩 얄라리 얄라 <br />
+                    <br />
+                    이럭저럭 하여 낮일랑 지내 왔건만 <br />
+                    올 이도 갈 이도 없는 밤일랑 또 어찌 할 것인가. <br />
+                    얄리얄리 얄라셩 얄라리 얄라 <br />
+                    <br />
+                    어디다 던지는 돌인가 누구를 맞히려던 돌인가. <br />
+                    미워할 이도 사랑할 이도 없이 맞아서 우노라. <br />
+                    얄리얄리 얄라셩 얄라리 얄라 <br />
+                    <br />
+                    살겠노라 살겠노라. 바다에 살겠노라. <br />
+                    나문재, 굴, 조개를 먹고 바다에 살겠노라. <br />
+                    얄리얄리 얄라셩 얄라리 얄라 <br />
+                    <br />
+                    가다가 가다가 듣노라. 에정지(미상) 가다가 듣노라. <br />
+                    사슴(탈 쓴 광대)이 솟대에 올라서 해금을 켜는 것을 듣노라. <br />
+                    얄리얄리 얄라셩 얄라리 얄라 <br />
+                    <br />
+                    가다 보니 배불룩한 술독에 독한 술을 빚는구나.<br />
+                    조롱박꽃 모양 누룩이 매워 (나를) 붙잡으니 내 어찌 하리이까.[1] <br />
+                    얄리얄리 얄라셩 얄라리 얄라 <br />
+                </div>
+
+                <img className={styles.detailVideoPreview} src="/images/video.png" alt="비디오프리뷰"/>
+
+                <div className={styles.detailContentGoodOrBad}>
+                    <div className={styles.detailBadContainer}>
+                        <img src="/images/bad.png" alt="싫어요"/>
+                        <div>24</div>
+                    </div>
+                    <div className={styles.detailGoodContainer}>
+                        <img src="/images/good.png" alt="좋아요"/>
+                        <div>12</div>
+                    </div>
+                </div>
+
+                <div className={styles.detailButtonContainer}>
+                    <button className={styles.detailButton}>
+                        <img src="/images/list_icon.png" alt="목록아이콘"/>
+                        <div>목록으로</div>
+                    </button>
+                    <button className={styles.detailButton}>
+                        <img src="/images/edit_icon.png" alt="수정아이콘"/>
+                        <div>수정하기</div>
+                    </button>
+                </div>                 
+            </div>
+        </div>
+    )
+}
+
+export default BoardsDetail;
