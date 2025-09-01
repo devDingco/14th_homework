b009f5fc12a36f66b662537e644e8160b834e1d6
KwonByungSeok <bryan3589@naver.com>
2025-09-01 17:59:23 +0900

    day19  done

---
+++ triptalk_next-app@14/my-app/src/app/boards/new/form-input.tsx
@@ -0,0 +1,68 @@
+import styles from "./styles.module.css";
+
+interface InputProps {
+  Input_Title?: React.ReactNode;
+  Input_Star?: React.ReactNode;
+  Input_Placeholder?: string;
+  onChange?: React.ChangeEventHandler;
+  errorMessage?: string;
+}
+
+export const SmallInput = (props: InputProps) => {
+  return (
+    <>
+      <div className={styles.게시글_인풋_작은거}>
+        <div className={styles.flex_row_gap4}>
+          {props.Input_Title}
+          <div className={styles.color_red}>{props.Input_Star}</div>
+        </div>
+        <input
+          type="text"
+          placeholder={props.Input_Placeholder}
+          className={styles.작은인풋}
+          onChange={props.onChange}
+        ></input>
+        <div className={styles.text_red}>{props.errorMessage}</div>
+      </div>
+    </>
+  );
+};
+
+export const LongInput = (props: InputProps) => {
+  return (
+    <>
+      <div className={styles.게시글_인풋_긴거}>
+        <div className={styles.flex_row_gap4}>
+          {props.Input_Title}
+          <div className={styles.color_red}>{props.Input_Star}</div>
+        </div>
+        <input
+          type="text"
+          placeholder={props.Input_Placeholder}
+          className={styles.긴인풋}
+          onChange={props.onChange}
+        ></input>
+        <div className={styles.text_red}>{props.errorMessage}</div>
+      </div>
+    </>
+  );
+};
+
+export const SuperLongInput = (props: InputProps) => {
+  return (
+    <>
+      <div className={styles.게시글_인풋_큰거}>
+        <div className={styles.flex_row_gap4}>
+          {props.Input_Title}
+          <div className={styles.color_red}>{props.Input_Star}</div>
+        </div>
+        <textarea
+          placeholder={props.Input_Placeholder}
+          className={styles.큰인풋}
+          onChange={props.onChange}
+        ></textarea>
+        <div className={styles.text_red}>{props.errorMessage}</div>
+      </div>
+    </>
+  );
+};
