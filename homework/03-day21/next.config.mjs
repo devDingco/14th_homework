0344ef25b080eb7b4c344d394d6d107adb01b72a
JY_Han <jy@JY-Hanui-MacBookAir.local>
2025-09-03 16:13:14 +0900

    Day22 과제

--- homework/03-day21/next.config.mjs
+++ homework/03-day21/next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode:false,
-    async rewrites() {
-        return [
-          {
-            source: "/graphql", // 브라우저에서 호출할 경로
-            destination: 'http://main-practice.codebootcamp.co.kr/graphql', // 실제 GraphQL 서버
-          },
-        ]
-      },
 };
 
 export default nextConfig;
