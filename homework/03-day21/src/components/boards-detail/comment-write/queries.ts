afd4a4eae46e05ced71f3f7dc3588c31bc6fa7a6
JY_Han <jy@JY-Hanui-MacBookAir.local>
2025-09-09 21:51:59 +0900

    Day26 과제- 댓글추가

---
+++ homework/03-day21/src/components/boards-detail/comment-write/queries.ts
@@ -0,0 +1,21 @@
+import { gql } from "@apollo/client";
+
+// 댓글 등록 Mutation
+export const CREATE_BOARD_COMMENT = gql`
+  mutation createBoardComment(
+    $boardId: ID!
+    $createBoardCommentInput: CreateBoardCommentInput!
+  ) {
+    createBoardComment(
+      boardId: $boardId
+      createBoardCommentInput: $createBoardCommentInput
+    ) {
+      _id
+      writer
+      contents
+      rating
+      createdAt
+    }
+  }
+`;
+
