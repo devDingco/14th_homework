667a6f731326c4b24ee07450d91548715271c00f
Daram0o0 <c30728535@gmail.com>
2025-09-12 09:57:52 +0900

    [v1] feat: 배너 설치

---
+++ 03_trip-trip/03_cna-v1/src/app/(sections)/HomeBanner/HomeBanner.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import styles from './styles.module.css'
+import { Autoplay, Pagination, Navigation } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+const IMAGE_SRC = [
+  { src: '/images/banner1.webp', alt: '배너이미지1' },
+  { src: '/images/banner2.webp', alt: '배너이미지2' },
+  { src: '/images/banner3.webp', alt: '배너이미지3' },
+] as const
+
+export default function HomeBanner() {
+  return (
+    <div>
+      <Swiper
+        slidesPerView={1}
+        loop
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {IMAGE_SRC.map((slide, idx) => {
+          return (
+            <SwiperSlide key={idx}>
+              <Image
+                src={slide.src}
+                alt={slide.alt}
+                width={1920}
+                height={515}
+                // className={styles.imageStatic}
+                // width={100}
+                // height={50}
+                className={styles.image_swiper} // object-fit 등 스타일
+              />
+            </SwiperSlide>
+          )
+        })}
+      </Swiper>
+    </div>
+  )
+}
