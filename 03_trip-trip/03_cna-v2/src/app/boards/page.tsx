c3c198ea996bd08de1194a640c94d6478a01aff5
Daram0o0 <c30728535@gmail.com>
2025-09-15 22:10:37 +0900

    [v2] feat(boards) : 페이지네이션 기능 추가

--- 03_trip-trip/03_cna-v2/src/app/boards/page.tsx
+++ 03_trip-trip/03_cna-v2/src/app/boards/page.tsx
@@ -1,12 +1,46 @@
 'use client'
 import BoardsListComponent from 'components/boards-list/list'
 import styles from './styles.module.css'
+import Pagination from 'components/boards-list/pagination'
+import { useQuery } from '@apollo/client'
+import {
+  FetchBoardsCountDocument,
+  FetchBoardsCountQuery,
+  FetchBoardsCountQueryVariables,
+  FetchBoardsDocument,
+  FetchBoardsQuery,
+  FetchBoardsQueryVariables,
+} from 'commons/graphql/graphql'
+import { useState } from 'react'
 
 export default function BoardsPage() {
+  const { data, refetch } = useQuery<FetchBoardsQuery, FetchBoardsQueryVariables>(
+    FetchBoardsDocument
+  )
+  const { data: dataBoardsCount } = useQuery<FetchBoardsCountQuery, FetchBoardsCountQueryVariables>(
+    FetchBoardsCountDocument
+  )
+  const lastPage = Math.ceil((dataBoardsCount?.fetchBoardsCount ?? 10) / 10)
+  const [currentPage, setCurrentPage] = useState(1)
+
+  if (!data || !dataBoardsCount) return <div>로딩 중 입니다</div>
+
   return (
     <div className={styles.detailLayout}>
       <div className={styles.detailBody}>
-        <BoardsListComponent />
+        <div className={styles.boardFrame}>
+          <BoardsListComponent
+            data={data}
+            dataBoardsCount={dataBoardsCount}
+            currentPage={currentPage}
+          />
+          <Pagination
+            refetch={refetch}
+            setCurrentPage={setCurrentPage}
+            currentPage={currentPage}
+            lastPage={lastPage}
+          />
+        </div>
       </div>
     </div>
   )
