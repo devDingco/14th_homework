2cd8599db74e13c197985b7dfbada59d84d26f2f
Daram0o0 <c30728535@gmail.com>
2025-09-20 16:59:56 +0900

    [v2] chore: 배지 프로젝트 기본 폴더 구조 초안 설정

---
+++ 03_trip-trip/03_cna-v2/src/components/admin-badges/admin-badge-form/index.tsx
@@ -0,0 +1,11 @@
+'use client'
+import styles from './styles.module.css'
+
+export default function AdminBadgeForm() {
+  return (
+    <div>
+      <div className={styles.enroll_title}>배지 등록</div>
+      <form></form>
+    </div>
+  )
+}
