8b7e20716dad855f004354aa72b94f03ead99310
Daram0o0 <c30728535@gmail.com>
2025-09-22 10:00:12 +0900

    [v2] feat: 배지 조회 페이지 생성

--- 03_trip-trip/03_cna-v2/src/components/admin-badges/admin-badge-list-item/index.tsx
+++ 03_trip-trip/03_cna-v2/src/components/admin-badges/admin-badge-list-item/index.tsx
@@ -1,11 +1,17 @@
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
+// TODO: 스타일 입히기
 export default function AdminBadgeListItem({ el }) {
-  console.log('🚀 ~ AdminBadgeListItem ~ el:', el)
+  // console.log('🚀 ~ AdminBadgeListItem ~ el:', el)
+  const router = useRouter()
+  const handleNavigate = (id) => {
+    router.push(`/admin/badges/${id}`)
+  }
 
-  const { name, description, image_url } = el
+  const { id, name, description, image_url } = el
   return (
-    <div>
+    <div style={{ border: 'solid 1px red', cursor: 'pointer' }} onClick={() => handleNavigate(id)}>
       <p>이름: {name}</p>
       <p>설명: {description}</p>
       <p>
