79f2b1351d77e2b4edc956b7fdcea6f99592c422
Daram0o0 <c30728535@gmail.com>
2025-09-23 23:02:44 +0900

    [v2] error: boards 페이지 뭉터기...

--- 03_trip-trip/03_cna-v2/src/components/boards-list/search/index.tsx
+++ 03_trip-trip/03_cna-v2/src/components/boards-list/search/index.tsx
@@ -2,28 +2,36 @@
 import { DatePicker } from 'antd'
 import styles from './styles.module.css'
 import { Search, DriveFileRenameOutline } from '@mui/icons-material'
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useEffect } from 'react'
 import _ from 'lodash'
-
 const { RangePicker } = DatePicker
 
 export default function BoardsSearch(props) {
-  const { data, refetch } = props
-  const [keyword, setKeyword] = useState('')
-
+  const { setKeyword, refetch } = props
+  // console.log('🚀 ~ BoardsSearch ~ keyword:', keyword)
   const getDebounce = _.debounce((value) => {
     refetch({
       search: value,
       page: 1,
     })
     setKeyword(value)
+    console.log('새로고침됨?')
   }, 500)
 
   const onChangeKeyword = (event: ChangeEvent<HTMLInputElement>) => {
-    getDebounce(event.target.value)
-  }
+    const keyword = event.target.value
+    console.log(keyword)
+    // 이게 검색 버튼을 누르면 검색이 돼야하는거 아니에요?
 
-  console.log('🚀 ~ BoardsSearch ~ keyword:', keyword)
+    // 근데 그러면 리패치를 하면 자자자
+
+    // 인풋에 입력을 해
+    // 그러면 리페치가 됩니다.
+
+    getDebounce(keyword)
+  }
+  console.log('리렌더링')
+
   return (
     <div className={styles.boardSearchLayout}>
       {/* 날짜 + 서치바 */}
@@ -41,6 +49,8 @@ export default function BoardsSearch(props) {
             placeholder="제목을 검색해 주세요."
             id="searchBar"
             onChange={onChangeKeyword}
+            // defaultValue={keyword}
+            // value={keyword}
           />
         </div>
 
