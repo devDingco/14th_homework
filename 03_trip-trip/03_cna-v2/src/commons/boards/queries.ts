8161679ea8d2f6a94f1a619a67ba39c3a63dcc22
Daram0o0 <c30728535@gmail.com>
2025-09-23 17:03:17 +0900

    [v2] feat(api): extend fetchBoards with startDate, endDate, search; update GQL and regenerate codegen

--- 03_trip-trip/03_cna-v2/src/commons/boards/queries.ts
+++ 03_trip-trip/03_cna-v2/src/commons/boards/queries.ts
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client'
 
 export const FETCH_BOARDS = gql`
-  query fetchBoards($page: Int) {
-    fetchBoards(page: $page) {
+  query fetchBoards($endDate: DateTime, $startDate: DateTime, $search: String, $page: Int) {
+    fetchBoards(endDate: $endDate, startDate: $startDate, search: $search, page: $page) {
       _id
       writer
       title
