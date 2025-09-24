88369ab47dfd4ff6063007fb55d03493b5b1909a
JY_Han <jy@JY-Hanui-MacBookAir.local>
2025-09-24 21:56:22 +0900

    Day 37 - 로그인

--- homework/03-day21/src/commons/settings/apollo-setting.tsx
+++ homework/03-day21/src/commons/settings/apollo-setting.tsx
@@ -3,16 +3,21 @@
 import { ApolloClient, ApolloLink, ApolloProvider, InMemoryCache } from '@apollo/client'
 import { ReactNode } from 'react';
 import createUploadLink from 'apollo-upload-client/createUploadLink.mjs'
+import { useAccessTokenStore } from '../stores/token-store';
 
 interface IApolloSetting {
     children: ReactNode;
   }
 
 
-export default function ApiUploadProvider(props: IApolloSetting){
+export default function ApiProvider(props: IApolloSetting){
+  const { accessToken } = useAccessTokenStore()
 
   const uploadLink = createUploadLink({
     uri: "http://main-practice.codebootcamp.co.kr/graphql",
+    headers: {
+      "Authorization": `Bearer ${accessToken}`,
+    }
   })
   
   const client = new ApolloClient({
