8f4a85498a9f71c7cec7c868fa77d51336814d55
Daram0o0 <c30728535@gmail.com>
2025-09-24 22:28:39 +0900

    [v2] feat: 회원가입 api 연동

--- 03_trip-trip/03_cna-v2/src/commons/utils/isEmptyObj.ts
+++ 03_trip-trip/03_cna-v2/src/commons/utils/isEmptyObj.ts
@@ -1,4 +1,4 @@
-function isEmptyObj(obj) {
+export const isEmptyObj = (obj) => {
   if (obj && typeof obj === 'object' && obj.constructor === Object) {
     return Object.keys(obj).length === 0
   }
--- 03_trip-trip/03_cna-v2/src/components/auth/register-form/hook.ts
+++ 03_trip-trip/03_cna-v2/src/components/auth/register-form/hook.ts
@@ -1,6 +1,14 @@
 'use client'
 
+import { useMutation } from '@apollo/client'
+import {
+  CreateUserDocument,
+  CreateUserMutation,
+  CreateUserMutationVariables,
+} from 'commons/graphql/graphql'
 import { registerValidation } from 'commons/libraries/validation/register-validation'
+import { isEmptyObj } from 'commons/utils/isEmptyObj'
+import { useRouter } from 'next/navigation'
 import { ChangeEvent, FormEvent, useState } from 'react'
 
 const initialState = {
@@ -11,17 +19,41 @@ const initialState = {
 }
 
 export const useRegister = () => {
+  const router = useRouter()
   const [registerValue, setRegisterValue] = useState(initialState)
   const [errors, setErrors] = useState({})
+
+  const [createUser] = useMutation<CreateUserMutation, CreateUserMutationVariables>(
+    CreateUserDocument
+  )
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { id, value } = event.target
     setRegisterValue((prev) => ({ ...prev, [id]: value }))
   }
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    setErrors(registerValidation(registerValue))
+
+    const nextErrors = registerValidation(registerValue)
+    setErrors(nextErrors)
+    if (!isEmptyObj(nextErrors)) return
+
+    try {
+      const { data } = await createUser({
+        variables: {
+          createUserInput: {
+            email: registerValue.email,
+            name: registerValue.name,
+            password: registerValue.password,
+          },
+        },
+      })
+      if (data?.createUser?._id) router.push('/login')
+    } catch (error) {
+      console.error('회원가입에 실패하였습니다:', error)
+    }
   }
+
   return { errors, registerValue, handleChange, handleSubmit }
 }
