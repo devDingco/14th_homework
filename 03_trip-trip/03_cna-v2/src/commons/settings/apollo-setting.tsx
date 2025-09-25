0585dbdb96a56b514246195048790dbb6cb7cfba
Daram0o0 <c30728535@gmail.com>
2025-09-25 21:15:11 +0900

    [v2] feat: 로컬스토리지에 accessToken 탑재

--- 03_trip-trip/03_cna-v2/src/commons/settings/apollo-setting.tsx
+++ 03_trip-trip/03_cna-v2/src/commons/settings/apollo-setting.tsx
@@ -3,12 +3,18 @@
 import { ApolloClient, ApolloLink, ApolloProvider, InMemoryCache } from '@apollo/client'
 import createUploadLink from 'apollo-upload-client/createUploadLink.mjs'
 import { useAccessTokenStore } from 'commons/stores/access-token-store'
+import { useEffect } from 'react'
 interface IApolloSetting {
   children: React.ReactNode
 }
 
 export default function ApiProvider({ children }: IApolloSetting) {
-  const { accessToken } = useAccessTokenStore()
+  const { accessToken, setAccessToken } = useAccessTokenStore()
+
+  useEffect(() => {
+    const result = localStorage.getItem('accessToken')
+    setAccessToken(result ?? '')
+  }, [])
 
   const uploadLink = createUploadLink({
     uri: 'http://main-practice.codebootcamp.co.kr/graphql',
