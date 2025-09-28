4276dbc4738c830121468c3fe8066059ad26c3cb
gharam12 <gharam12@gharam12ui-MacBookAir.local>
2025-09-28 23:01:51 +0900

    [250928]-day38-과제제출

--- 03-maincamp/homework/src/commons/layout/banner/index.tsx
+++ 03-maincamp/homework/src/commons/layout/banner/index.tsx
@@ -6,8 +6,13 @@ import Slider from "react-slick";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+
 
 export default function LayoutBanner() {
+  const router = useRouter();
   const settings = {
     dots: true,
     infinite: true,
@@ -15,7 +20,17 @@ export default function LayoutBanner() {
     slidesToShow: 1,
     slidesToScroll: 1
   };
+
+  useEffect(() => {
+    if (!localStorage.getItem("accessToken")) {
+      alert("로그인후 이용가능합니다");
+      router.push("/login");
+    }
+  }, []);
+
   return (
+
+
     <div className={styles.slider__container}>
 
     <Slider {...settings}>
