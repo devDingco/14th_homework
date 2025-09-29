6b7688779ce8ce8665e5a705be32d3b52ae3a99f
Daram0o0 <c30728535@gmail.com>
2025-09-29 21:10:31 +0900

    [v2] feat: withAuth 훅에 제네릭 타입 적용

--- 03_trip-trip/03_cna-v2/src/commons/hocs/withAuth.tsx
+++ 03_trip-trip/03_cna-v2/src/commons/hocs/withAuth.tsx
@@ -2,25 +2,27 @@ import { isTokenExpired } from 'commons/utils/auth'
 import { useRouter } from 'next/navigation'
 import { useEffect, useRef } from 'react'
 
-export const withAuth = (컴포넌트) => (props) => {
-  const router = useRouter()
-  const firedRef = useRef(false)
-  useEffect(() => {
-    if (firedRef.current) return
-    firedRef.current = true
+export const withAuth =
+  <P extends object>(컴포넌트: React.ComponentType<P>) =>
+  (props: P) => {
+    const router = useRouter()
+    const firedRef = useRef(false)
+    useEffect(() => {
+      if (firedRef.current) return
+      firedRef.current = true
 
-    const token = localStorage.getItem('accessToken') ?? undefined
-    if (!token) {
-      alert('로그인 후 서비스를 이용해주세요.')
-      router.replace('/login')
-      return
-    }
-    if (isTokenExpired(token)) {
-      alert('로그인이 만료되었습니다. 로그인 후 다시 이용해주세요.')
-      localStorage.removeItem('accessToken')
-      router.push('/login')
-    }
-  }, [router])
+      const token = localStorage.getItem('accessToken') ?? undefined
+      if (!token) {
+        alert('로그인 후 서비스를 이용해주세요.')
+        router.replace('/login')
+        return
+      }
+      if (isTokenExpired(token)) {
+        alert('로그인이 만료되었습니다. 로그인 후 다시 이용해주세요.')
+        localStorage.removeItem('accessToken')
+        router.push('/login')
+      }
+    }, [router])
 
-  return <컴포넌트 {...props} />
-}
+    return <컴포넌트 {...props} />
+  }
