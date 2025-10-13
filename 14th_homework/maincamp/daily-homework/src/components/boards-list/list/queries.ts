1b06f14f862da52abd1f84fef54112beb21e0212
jang won <cjw7378@gmail.com>
2025-10-13 18:48:13 +0900

    [251013] Day44 과제제출

--- 14th_homework/maincamp/daily-homework/src/components/boards-list/list/queries.ts
+++ 14th_homework/maincamp/daily-homework/src/components/boards-list/list/queries.ts
@@ -4,8 +4,8 @@ import { gql } from '@apollo/client';
 
 // page를 가져와야 하기 때문에 fetchBoards로 수정
 export const FETCH_BOARDS = gql`
-  query fetchBoards($page: Int $search: String) {
-    fetchBoards(page: $page search: $search) {
+  query fetchBoards($page: Int, $search: String) {
+    fetchBoards(page: $page, search: $search) {
       _id
       writer
       title
@@ -15,8 +15,8 @@ export const FETCH_BOARDS = gql`
 `;
 
 export const FETCH_BOARDS_COUNT = gql`
-  query fetchBoardsCount {
-    fetchBoardsCount
+  query fetchBoardsCount($search: String) {
+    fetchBoardsCount(search: $search)
   }
 `;
 
